6881277e0e877034409b774de38d17a417545783
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-11-15 13:37:52 +0100

    Update the database.

--- buy-my-house.api/init-database.sql
+++ buy-my-house.api/init-database.sql
@@ -1,3 +1,7 @@
--- init-database.sql
-CREATE DATABASE buymyhousedb;
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'buymyhousedb')
+BEGIN
+    CREATE DATABASE buymyhousedb;
+    -- Additional initialization logic for the database can go here
+END
 GO
+
--- buy-my-house.api/obj/Debug/net8.0/buy-my-house.api.AssemblyInfo.cs
+++ buy-my-house.api/obj/Debug/net8.0/buy-my-house.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("buy-my-house.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ad370c7b4268cf9d8cf152e8e38bbbfc2d2cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097fe4d30608a824b2f88d83ef08ff86fecf8e8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("buy-my-house.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("buy-my-house.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
