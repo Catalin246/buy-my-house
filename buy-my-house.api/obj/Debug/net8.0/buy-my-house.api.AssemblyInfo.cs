969dde9a96ca2d0000e7ddaea216bb52a669804e
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-11-16 14:03:41 +0100

    Add debug log.

--- buy-my-house.api/obj/Debug/net8.0/buy-my-house.api.AssemblyInfo.cs
+++ buy-my-house.api/obj/Debug/net8.0/buy-my-house.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("buy-my-house.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ba82feea0fc266f16da0af8acdf8d83f49c70c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9c277947ba6ce5ca0dff46155a7e866c8132a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("buy-my-house.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("buy-my-house.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
